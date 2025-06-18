4e6de8473e4a1a6a2c08af8aa82e8a3353bbe2db
sheicodev <bao13a11@gmail.com>
2025-06-18 10:22:18 +0700

    bao

--- backend/account.php
+++ backend/account.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+$user = $_SESSION['user_profile'] ?? null;
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,28 +25,72 @@
             </div>
         </div>
     </div>
-    <div id="container">
-        <div id="sidebar">
-            <div class="sidebar-item"></div>
-            <div class="sidebar-item"></div>
-            <div class="sidebar-item"></div>
-            <div class="sidebar-item"></div>
-            <div class="sidebar-item"></div>
-            <div class="sidebar-item"></div>
+    <div class="container">
+        <div class="sidebar">
+            <h2>Account<br>Management</h2>
+            <ul>
+            <li>YOUR NAME</li>
+            <li>PERSONAL INFORMATION</li>
+            <li>USER ACCOUNT SIGN-IN</li>
+            <li>CONNECTED ACCOUNTS</li>
+            <li>MULTI-FACTOR AUTHENTICATION</li>
+            <li>LOGIN MANAGEMENT</li>
+            <li>COMMUNICATION PREFERENCES</li>
+            </ul>
         </div>
-        <div id="main-content">
-            <div class="content-box"></div>
-            <div class="content-box"></div>
-            <div class="content-box"></div>
-            <div class="content-box"></div>
-            <div class="content-box"></div>
-            <div class="content-box"></div>
-            <div class="content-box"></div>
-            <div class="content-box"></div>
-            <div class="content-box"></div>
-            <div class="content-box"></div>
-            <div class="content-box"></div>
-            <div class="content-box"></div>
+
+        <div class="main-content">
+            <!-- Riot ID box -->
+            <div class="box">
+            <h3>Your Name</h3>
+            <p>Your name is used by players to find you through the social panel in our apps.</p>
+            <div class="input-group">
+                <div class="input-field">
+                <label>Your Name</label>
+                <input type="text" placeholder="Enter your name pls" value="<?= isset($_SESSION['user_profile']['name']) ? htmlspecialchars($_SESSION['user_profile']['name']) : '' ?>">
+                </div>
+                <div class="input-field">
+                <label>TAGLINE</label>
+                <input type="text" placeholder="Enter your tag pls">
+                </div>
+            </div>
+            <button class="save-btn">SAVE CHANGES</button>
+            </div>
+
+            <!-- Personal Info box -->
+            <div class="box">
+            <h3>Personal Information</h3>
+            <p>This information is private and will not be shared with other players. Read the 
+                <a href="#">Riot Privacy Notice</a> anytime!</p>
+            <div class="input-group">
+                <div class="input-field">
+                <label>EMAIL ADDRESS</label>
+                <input type="text" placeholder="bao*****@gm***.com" value="<?= isset($_SESSION['user_profile']['email']) ? htmlspecialchars($_SESSION['user_profile']['email']) : '' ?>">
+                </div>
+                <div class="input-field">
+                <label>COUNTRY / REGION</label>
+                <input type="text" value="VNM">
+                </div>
+            </div>
+            <button class="save-btn">SAVE AND VERIFY</button>
+            </div>
+
+            <!-- User Account sign in -->
+            <div class="box">
+            <h3>User Account Sign-in</h3>
+            <p>This information is private and will not be shared with other players. Read the <a href="#">Riot Privacy Notice</a> anytime!</p>
+            <div class="input-group">
+                <div class="input-field">
+                <label>EMAIL ADDRESS</label>
+                <input type="text" placeholder="bao*****@gm***.com" value="<?= isset($_SESSION['user_profile']['email']) ? htmlspecialchars($_SESSION['user_profile']['email']) : '' ?>">
+                </div>
+                <div class="input-field">
+                <label>COUNTRY / REGION</label>
+                <input type="text" value="VNM">
+                </div>
+            </div>
+            <button class="save-btn">SAVE AND VERIFY</button>
+            </div>
         </div>
     </div>
 </body>
